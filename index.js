e3989b428a8f03be5a7d9484473bc51b64048224
oak-and-lion <oakandlionsociety@gmail.com>
2020-11-29 16:20:50 -0500

    updates

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-new Vue({
+var hello = new Vue({
     el: "#hello-world-app",
     data: {
-        msg: "Vue js Hello World - Your First Vue.js App"
-    }
+        msg: "Vue js Hello World - Your First Vue.js App",
+      message: 'Welcome Engineering Training',
+      result: '',
+      responseAvailabe: false
+    },
+    methods: {
+     fetchAPIData(func,) {
+        this.responseAvailable = false;
+        fetch("https://childsheartyoga.com/api.aspx?pf=" + func, {
+            "method": "GET",
+            "headers": {
+            }
+        })
+        .then(response => { 
+            if(response.ok){
+                return response.json()    
+            } else{
+                alert("Server returned " + response.status + " : " + response.statusText);
+            }
+        })
+        .then(response => {
+            this.result = response.users; 
+            this.responseAvailable = true;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+     } //fetchAPIData
+    } //methods
   });
